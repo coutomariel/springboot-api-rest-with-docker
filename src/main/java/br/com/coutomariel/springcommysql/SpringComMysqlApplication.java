53dd771eb612e356f38966b2ee960b38cb96f06a
coutomariel <coutomariel@gmail.com>
2021-02-16 15:31:28 -0300

    First commit

---
+++ src/main/java/br/com/coutomariel/springcommysql/SpringComMysqlApplication.java
@@ -0,0 +1,13 @@
+package br.com.coutomariel.springcommysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringComMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringComMysqlApplication.class, args);
+	}
+
+}
